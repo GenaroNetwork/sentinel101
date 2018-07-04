b2e689a4ecc50298c05635e9447f2c29cda8c8a7
lishi <lishi1608@gmail.com>
2018-07-04 12:37:33 +0800

    init
    Signed-off-by: lishi <lishi1608@gmail.com>

---
+++ s101-ui/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Genaro Sharer 排行</h1>
+    <Join />
+    <Statistics />
+    <Farmer />
+  </div>
+</template>
+
+<script>
+import Join from './components/Join.vue'
+import Statistics from './components/Statistics.vue'
+import Farmer from './components/Farmer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Join,
+    Statistics,
+    Farmer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
