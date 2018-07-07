3bc9bf596def4aec4abb21eb5a6c283b9f8888e2
lishi <lishi1608@gmail.com>
2018-07-07 11:42:43 +0800

    add search
    Signed-off-by: lishi <lishi1608@gmail.com>

---
+++ s101-ui/src/components/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-form :inline="true" :model="formInline" class="demo-form-inline">
+    <el-form-item label="">
+      <el-input v-model="formInline.user" placeholder="请输入钱包地址"></el-input>
+    </el-form-item>
+    <el-form-item label="">
+      <el-input v-model="formInline.nickname" placeholder="昵称"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">加入 Genaro Sharer 全球排名</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+
+import {register} from '../api/topFarmer'
+  export default {
+    data() {
+      return {
+        formInline: {
+          user: '',
+          nickname: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+* {
+  text-align: center;
+}
+</style>
