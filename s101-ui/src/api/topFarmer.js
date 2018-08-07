f1e06e1886874306a18df03b8bc2bf9c4e947c10
lishi <lishi1608@gmail.com>
2018-08-07 18:42:54 +0800

    remove join
    Signed-off-by: lishi <lishi1608@gmail.com>

--- s101-ui/src/api/topFarmer.js
+++ s101-ui/src/api/topFarmer.js
@@ -2,7 +2,7 @@ import * as axios from 'axios'
 import { isAddress } from 'web3-utils'
 
 const fetchUrl = "http://118.31.61.119:8000/top-farmer"
-const registerUrl = "http://118.31.61.119:8000/register-farmer"
+// const registerUrl = "http://118.31.61.119:8000/register-farmer"
 //const totalStakeUrl = "http://118.31.61.119:8000/total-stake"
 //const farmerStakeUrl = "http://118.31.61.119:8000/stake/"
 const farmerOutline = "http://118.31.61.119:8000/farmer-outline"
@@ -19,31 +19,11 @@ async function getTopN(address) {
     return (await axios.get(url)).data;
 }
 
-async function register (address, nickName) {
-    if(!address) {
-        throw new Error('missing address')
-    }
-    if(!nickName) {
-        throw new Error('missing nickName')
-    }
-    if(nickName.length > 10) {
-        throw new Error('nickName too long')
-    }
-    if(!isAddress(address)) {
-        throw new Error('invalid address')
-    }
-
-    await axios.post(registerUrl, {
-        address, nickName
-    })
-}
-
 async function getFarmerOutline() {
     return (await axios.get(farmerOutline)).data
 }
 
 export {
     getTopN,
-    register,
     getFarmerOutline
 }
